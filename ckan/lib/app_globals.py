2f75953eb477aa9b919febbda513bb3f5d8b9b20
Momin Imtiaz <mominimtiaz0786@gmail.com>
2025-07-29 13:41:38 +0500

    updated

--- ckan/lib/app_globals.py
+++ ckan/lib/app_globals.py
@@ -64,7 +64,7 @@ app_globals_from_config_details: dict[str, dict[str, str]] = {
     'ckan.tracking_enabled' : {'default': 'false', 'type' : 'bool'},
 
     # int
-    'ckan.datasets_per_page': {'default': '20', 'type': 'int'},
+    'ckan.datasets_per_page': {'default': '24', 'type': 'int'},
     'ckan.activity_list_limit': {'default': '30', 'type': 'int'},
     'ckan.user_list_limit': {'default': '20', 'type': 'int'},
     'search.facets.default': {'default': '10', 'type': 'int',
